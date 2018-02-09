9f2b50857799224ae13fb1153c7ea5214e346e7e
KennyBones <raziel014@gmail.com>
2018-02-10 00:38:19 +0100

    DB update

--- craft/config/db.php
+++ craft/config/db.php
@@ -33,11 +33,11 @@ $customDbConfig = array(
 		'database' 	=> ''
 	),
 	'PROD' => array(
-		'server' 	=> '',
-		'user' 		=> '',
-		'password' 	=> '',
-		'database' 	=> '',
-		//'initSQLs' 	=> array("SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';")
+		'server' 	=> 'mysql193383.mysql.sysedata.no',
+		'user' 		=> 'mysql193383',
+		'password' 	=> 'Mester11',
+		'database' 	=> 'mysql193383',
+		'initSQLs' 	=> array("SET SESSION sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';")
 	)
 );
 
